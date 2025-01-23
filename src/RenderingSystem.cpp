579843739f38d2de5ac63a216106f75ab5aed185
Jon <jonathan.mulyk@ucalgary.ca>
2025-01-22 22:49:21 -0600

    modify shaders to color triangle

--- src/RenderingSystem.cpp
+++ src/RenderingSystem.cpp
@@ -34,12 +34,22 @@ RenderingSystem::RenderingSystem(const int width, const int height)
 	// Shader
 //	Shader shader("../assets/shaders/VertShader.vert", "../assets/shaders/FragShader.frag");
 	//this->shader("", "");
-
+	
+	/*
 	float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
 	 0.5f, -0.5f, 0.0f,
 	 0.0f,  0.5f, 0.0f
 	};
+	*/
+
+	float vertices[] = {
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+	};
+
 
 	glGenVertexArrays(1, &this->VAO);
 	glGenBuffers(1, &this->VBO);
@@ -49,12 +59,17 @@ RenderingSystem::RenderingSystem(const int width, const int height)
 	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	glBindVertexArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//glBindVertexArray(0);
+
+	//unsigned int testTriangleVAO = initVAO(vertices, sizeof(vertices));
 }
 
 
