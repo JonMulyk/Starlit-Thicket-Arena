6e2f2922a87450b52118923f660d9d6b06763828
Ahmad <ahmad.janjua@ucalgary.ca>
2025-02-01 08:40:55 -0700

    Basic File refactoring + Time Class
    - moved all libraries into single folder
        - including physx
    - moved all the build files into a single folder and put it in git ignore.
    NOTE: the dlls will need to be placed inside the build folder x64/(release|debug) folder <- same as before but in one place
    - created project folder with all the working files like source and header and asset files.
    
    +++
    - Created a simple time in seconds class

---
+++ project/src/TimeSeconds.cpp
@@ -0,0 +1,35 @@
+#include "TimeSeconds.h"
+
+// Ctor
+TimeSeconds::TimeSeconds() : m_currentTime(glfwGetTime()) {}
+
+void TimeSeconds::tick() {
+    // Get the new time.
+    m_newTime = glfwGetTime();
+
+    // Update values.
+    m_frameTime = m_newTime - m_currentTime;
+    m_accumulator += m_frameTime;
+    m_currentTime = m_newTime;
+}
+
+void TimeSeconds::advance() {
+    m_accumulator -= timeStep;
+    m_elapsedTime += timeStep;
+}
+
+double TimeSeconds::getCurrentTime() const {
+    return m_currentTime;
+}
+
+double TimeSeconds::getFrameTime() const {
+    return m_frameTime;
+}
+
+double TimeSeconds::getAccumultor() const {
+    return m_accumulator;
+}
+
+double TimeSeconds::getElapsedTime() const {
+    return m_elapsedTime;
+}
