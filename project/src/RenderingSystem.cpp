2082a2f8f6e9b22f8dccbef3b14d05019b138b91
derekhung232 <60674701+derekhung232@users.noreply.github.com>
2025-03-05 16:05:28 -0700

    Made ground texture bigger

--- project/src/RenderingSystem.cpp
+++ project/src/RenderingSystem.cpp
@@ -91,7 +91,7 @@ void RenderingSystem::renderScene(std::vector<Model>& sceneModels)
 
 
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::scale(model, glm::vec3(10.0f, 1.0f, 10.0f)); // Scale the ground
+    model = glm::scale(model, glm::vec3(23.0f, 1.0f, 23.0f)); // Scale the ground
     model = glm::translate(model, glm::vec3(4.5f, 0.0f, 4.5f)); // translate ground
     //model = glm::rotate(model, glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // Rotate 180 degrees around X-axis
     sceneModels[0].getShader().setMat4("model", model);
