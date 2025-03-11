f99f4c5022546a0d8ec72899604b159fe6fe8639
DuroMonstahCardo <efren.garcia6969@gmail.com>
2025-03-11 11:27:24 -0600

    changed button and isClicked into a single class
    reusable button function now for other settings if needed

--- project/include/Button.h
+++ project/include/Button.h
@@ -1,1 +1,70 @@
 #pragma once
+#include <GLFW/glfw3.h>
+#include "stb_image.h"
+#include "Shader.h"
+#include "TTF.h"
+
+class Button {
+public:
+    Button(float x, float y, float width, float height, glm::vec3 color)
+        : x(x), y(y), width(width), height(height), color(color) {}
+
+    void draw(Shader* shader, int windowWidth, int windowHeight) {
+        auto convertX = [windowWidth](float xPos) -> float {
+            return (2.0f * xPos / windowWidth) - 1.0f;
+        };
+        auto convertY = [windowHeight](float yPos) -> float {
+            return 1.0f - (2.0f * yPos / windowHeight);
+        };
+
+        float vertices[] = {
+            convertX(x), convertY(y), 0.0f, 0.0f,
+            convertX(x), convertY(y + height), 0.0f, 0.0f,
+            convertX(x + width), convertY(y + height), 0.0f, 0.0f,
+            convertX(x), convertY(y), 0.0f, 0.0f,
+            convertX(x + width), convertY(y + height), 0.0f, 0.0f,
+            convertX(x + width), convertY(y), 0.0f, 0.0f
+        };
+
+        unsigned int VBO, VAO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        shader->use();
+        shader->setInt("useTexture", 0);
+        shader->setVec3("solidColor", color);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glBindVertexArray(0);
+        glDeleteBuffers(1, &VBO);
+        glDeleteVertexArrays(1, &VAO);
+    }
+
+    bool isClicked(double mouseX, double mouseY, int windowWidth, int windowHeight) {
+        float normX = mouseX / windowWidth;
+        float normY = mouseY / windowHeight;
+
+        float buttonNormX = x / windowWidth;
+        float buttonNormY = y / windowHeight;
+        float buttonNormW = width / windowWidth;
+        float buttonNormH = height / windowHeight;
+
+        return normX >= buttonNormX && normX <= (buttonNormX + buttonNormW) &&
+            normY >= buttonNormY && normY <= (buttonNormY + buttonNormH);
+    }
+
+
+private:
+    float x, y, width, height;
+    glm::vec3 color;
+};
--- project/include/MainMenu.h
+++ project/include/MainMenu.h
@@ -2,15 +2,21 @@
 #include "stb_image.h"
 #include "Shader.h"
 #include "TTF.h"
+#include <Button.h>
+
 
 class MainMenu {
 public:
-    MainMenu(Windowing& window, TTF& textRenderer) : window(window), textRenderer(textRenderer) {
+    MainMenu(Windowing& window, TTF& textRenderer)
+        : window(window), textRenderer(textRenderer),
+        startButton(0, 0, 0, 0, glm::vec3(0, 0, 0)), 
+        exitButton(0, 0, 0, 0, glm::vec3(0, 0, 0)) 
+    {
         compileShaders();
         loadBackgroundTexture();
         initializeUIText();
+        initializeButtons(); 
     }
-
 
     ~MainMenu() {
         glDeleteTextures(1, &backgroundTexture);
@@ -28,27 +34,13 @@ public:
             if (glfwGetMouseButton(window.getGLFWwindow(), GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
                 double xpos, ypos;
                 glfwGetCursorPos(window.getGLFWwindow(), &xpos, &ypos);
-
-                // Get current window size
-                
                 glfwGetWindowSize(window.getGLFWwindow(), &windowWidth, &windowHeight);
 
-                //button bounds
-                float startX = 0.5f * windowWidth - (0.1f * windowWidth);
-                float startY = 0.5f * windowHeight - (0.03125f * windowHeight);
-                float startWidth = 0.2f * windowWidth;
-                float startHeight = 0.0625f * windowHeight;
-                if (isButtonClicked(xpos, ypos, startX, startY, startWidth, startHeight)) {
+                if (startButton.isClicked(xpos, ypos, windowWidth, windowHeight)) {
                     inMenu = false;
                     glfwSetInputMode(window.getGLFWwindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
                 }
-
-                //button bounds for exit game
-                float exitX = 0.5f * windowWidth - (0.1f * windowWidth);
-                float exitY = startY + startHeight + (0.15f * windowHeight);
-                float exitWidth = 0.2f * windowWidth;
-                float exitHeight = 0.0625f * windowHeight;
-                if (isButtonClicked(xpos, ypos, exitX, exitY, exitWidth, exitHeight)) {
+                if (exitButton.isClicked(xpos, ypos, windowWidth, windowHeight)) {
                     glfwSetWindowShouldClose(window.getGLFWwindow(), true);
                 }
             }
@@ -58,79 +50,57 @@ public:
 private:
     std::vector<Text> uiText;
     Windowing& window;
-    Shader* shader; // Use Shader class instead of raw shader program
+    Shader* shader;
     unsigned int backgroundTexture;
     TTF& textRenderer;
     int windowWidth, windowHeight;
+    Button startButton, exitButton;
 
     void renderMenu() {
         glDisable(GL_DEPTH_TEST);
         drawBackground();
 
-        int windowWidth, windowHeight;
         glfwGetWindowSize(window.getGLFWwindow(), &windowWidth, &windowHeight);
 
-        float startX = 0.5f * windowWidth - (0.1f * windowWidth);
-        float startY = 0.5f * windowHeight - (0.03125f * windowHeight);
-        float startWidth = 0.2f * windowWidth;
-        float startHeight = 0.0625f * windowHeight;
-        drawButton(startX, startY, startWidth, startHeight);
 
-        float exitX = 0.5f * windowWidth - (0.1f * windowWidth);
-        float exitY = startY + startHeight + (0.15f * windowHeight);
-        float exitWidth = 0.2f * windowWidth;
-        float exitHeight = 0.0625f * windowHeight;
-        drawButton(exitX, exitY, exitWidth, exitHeight);
+        startButton = Button(0.4f * windowWidth, 0.5f * windowHeight, 0.2f * windowWidth, 0.0625f * windowHeight, glm::vec3(1, 0, 0));
+        exitButton = Button(0.4f * windowWidth, 0.65f * windowHeight, 0.2f * windowWidth, 0.0625f * windowHeight, glm::vec3(1, 0, 0));
 
-        this->renderText(uiText);
+        startButton.draw(shader, windowWidth, windowHeight);
+        exitButton.draw(shader, windowWidth, windowHeight);
+
+        renderText(uiText);
     }
+
 
     void initializeUIText() {
         glfwGetWindowSize(window.getGLFWwindow(), &windowWidth, &windowHeight);
 
-        // Align text with the "Start" button
-        float buttonX = 0.5f * windowWidth;  // Center X
-        float buttonY = 0.5f * windowHeight; // Center Y
+        float buttonX = 0.5f * windowWidth;
+        float buttonY = 0.5f * windowHeight;
 
-        Text Start = Text("Start", buttonX + 60, buttonY + 200.0f, 1.0f, glm::vec3(1, 1, 1));
-        Text Exit = Text("Exit", buttonX + 68.0f, buttonY - 100, 1.0f, glm::vec3(1, 1, 1));
+        Text Start = Text("Start", buttonX + 60, buttonY + 160.0f, 1.0f, glm::vec3(1, 1, 1));
+        Text Exit = Text("Exit", buttonX + 70.0f, buttonY - 60, 1.0f, glm::vec3(1, 1, 1));
 
         uiText.push_back(Start);
         uiText.push_back(Exit);
     }
 
+    void initializeButtons() {
+        glfwGetWindowSize(window.getGLFWwindow(), &windowWidth, &windowHeight);
+        startButton = Button(0.4f * windowWidth, 0.5f * windowHeight, 0.2f * windowWidth, 0.0625f * windowHeight, glm::vec3(1, 0, 0));
+        exitButton = Button(0.4f * windowWidth, 0.65f * windowHeight, 0.2f * windowWidth, 0.0625f * windowHeight, glm::vec3(1, 0, 0));
+    }
 
     void renderText(const std::vector<Text>& renderingText) {
-        for (const auto& text : renderingText)
-        {
+        for (const auto& text : renderingText) {
             textRenderer.render(text.getTextToRender(), text.getX(), text.getY(), text.getScale(), text.getColor());
         }
     }
 
-
-    void loadBackgroundTexture() {
-        glGenTextures(1, &backgroundTexture);
-        glBindTexture(GL_TEXTURE_2D, backgroundTexture);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        int width, height, nrChannels;
-        stbi_set_flip_vertically_on_load(true);
-        unsigned char* data = stbi_load("project/assets/background/backgrounMainMenu.jpg", &width, &height, &nrChannels, 0);
-        if (data) {
-            GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
-            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
-        }
-        else {
-            printf("Failed to load background texture\n");
-        }
-        stbi_image_free(data);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        stbi_set_flip_vertically_on_load(false);
+    void compileShaders() {
+        //shader class to load shaders
+        shader = new Shader("MainMenuShader", "project/assets/shaders/mainMenuShader.vert", "project/assets/shaders/mainMenuShader.frag");
     }
 
     void drawBackground() {
@@ -157,7 +127,7 @@ private:
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
         glEnableVertexAttribArray(1);
 
-        shader->use();  
+        shader->use();
         shader->setInt("useTexture", 1);
         glBindTexture(GL_TEXTURE_2D, backgroundTexture);
         glBindVertexArray(VAO);
@@ -169,57 +139,29 @@ private:
         glDeleteVertexArrays(1, &VAO);
     }
 
-    void drawButton(int x, int y, int width, int height) {
-        int windowWidth, windowHeight;
-        glfwGetWindowSize(window.getGLFWwindow(), &windowWidth, &windowHeight);
+    void loadBackgroundTexture() {
+        glGenTextures(1, &backgroundTexture);
+        glBindTexture(GL_TEXTURE_2D, backgroundTexture);
 
-        auto convertX = [windowWidth](float xPos) -> float {
-            return (2.0f * xPos / windowWidth) - 1.0f;
-        };
-        auto convertY = [windowHeight](float yPos) -> float {
-            return 1.0f - (2.0f * yPos / windowHeight);
-        };
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        float vertices[] = {
-            convertX(x),           convertY(y),           0.0f, 0.0f,  // top-left
-            convertX(x),           convertY(y + height),  0.0f, 0.0f,  // bottom-left
-            convertX(x + width),   convertY(y + height),  0.0f, 0.0f,  // bottom-right
-            convertX(x),           convertY(y),           0.0f, 0.0f,  // top-left
-            convertX(x + width),   convertY(y + height),  0.0f, 0.0f,  // bottom-right
-            convertX(x + width),   convertY(y),           0.0f, 0.0f   // top-right
-        };
-
-        unsigned int VBO, VAO;
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-        glEnableVertexAttribArray(1);
-
-        shader->use(); 
-        shader->setInt("useTexture", 0);
-        shader->setVec3("solidColor", glm::vec3(1.0f, 0.0f, 0.0f)); //red color for now
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        glBindVertexArray(0);
-        glDeleteBuffers(1, &VBO);
-        glDeleteVertexArrays(1, &VAO);
-    }
-
-    bool isButtonClicked(double mouseX, double mouseY, int btnX, int btnY, int btnWidth, int btnHeight) {
-        return mouseX >= btnX && mouseX <= btnX + btnWidth &&
-            mouseY >= btnY && mouseY <= btnY + btnHeight;
-    }
-
-    void compileShaders() {
-        //shader class to load shaders
-        shader = new Shader("MainMenuShader", "project/assets/shaders/mainMenuShader.vert", "project/assets/shaders/mainMenuShader.frag");
+        int width, height, nrChannels;
+        stbi_set_flip_vertically_on_load(true);
+        unsigned char* data = stbi_load("project/assets/background/backgrounMainMenu.jpg", &width, &height, &nrChannels, 0);
+        if (data) {
+            GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else {
+            printf("Failed to load background texture\n");
+        }
+        stbi_image_free(data);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        stbi_set_flip_vertically_on_load(false);
     }
 };
+
