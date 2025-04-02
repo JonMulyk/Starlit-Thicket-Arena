25c50c4a9593fc18552832c72b39ef5f82b58e8a
Jon <jonathan.mulyk@ucalgary.ca>
2025-04-01 21:53:24 -0600

    Update Game UI text to scale with resolution

--- project/src/UIManager.cpp
+++ project/src/UIManager.cpp
@@ -5,15 +5,24 @@
 UIManager::UIManager(int windowWidth, int windowHeight)
 	: windowWidth(windowWidth), windowHeight(windowHeight)
 {
+    this->textScale = std::min(windowWidth, windowHeight) * 0.0010f;
     initializeUIText();
 }
 
 void UIManager::initializeUIText()
 {
-    Text uiFPS = Text("", 10.0f, 1410.0f, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
-    Text roundTimeLeft = Text("roundTimeLeft", "0:0", static_cast<float>(windowWidth / 2), 1390.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
-    Text fuel = Text("fuel", "100", 0, 1, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
-    Text score = Text("SCORE", 10.0f, 1370.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+
+    float width = static_cast<float>(windowWidth);
+    float height = static_cast<float>(windowHeight);
+    float topOfScreenY = height - (height * 0.045f);
+    float farLeftX = width * 0.0055f;
+
+    glm::vec3 textColor = glm::vec3(0.5f, 0.8f, 0.2f);
+
+    Text uiFPS = Text("", farLeftX, topOfScreenY, 0.5f, textColor, TTF::TEXT_POSITION::LEFT);
+    Text roundTimeLeft = Text("roundTimeLeft", "0:0", width / 2.0f, topOfScreenY, this->textScale, textColor, TTF::TEXT_POSITION::CENTER);
+    Text fuel = Text("fuel", "100", farLeftX, height*0.01f, this->textScale, textColor, TTF::TEXT_POSITION::LEFT);
+    Text score = Text("SCORE", farLeftX, height * 0.858f, this->textScale, textColor, TTF::TEXT_POSITION::LEFT);
 
     uiText.push_back(uiFPS);
     uiText.push_back(roundTimeLeft);
@@ -26,8 +35,12 @@ void UIManager::addScoreText(GameState& gameState)
     //Clear old score texts
     uiText.erase(uiText.begin() + 4, uiText.end());
 
-    float scoreHeight = 1300.0f;
-    float heightIncrementOffset = 50.0f;
+    //float scoreHeight = 1300.0f;
+    float scoreHeight = static_cast<float>(windowHeight) * 0.8085f;
+
+    float heightIncrementOffset = static_cast<float>(windowHeight) * 0.0417f;
+
+    float farLeftX = static_cast<float>(windowWidth) * 0.055f;
 
     for (const auto& score : gameState.getSortedScores())
     {
@@ -37,8 +50,10 @@ void UIManager::addScoreText(GameState& gameState)
         //std::string scoreText = score.first + ":" + std::to_string(score.second);
         std::string scoreText = oss.str();
 
+		float farLeftX = static_cast<float>(windowWidth) * 0.0055f;
+
         // adding text to the vector
-        uiText.push_back(Text(scoreText, 10.0f, scoreHeight, 1.0f, glm::vec3(0.5, 0.8f, 0.2f)));
+        uiText.push_back(Text(scoreText, farLeftX, scoreHeight, this->textScale * 0.8f, glm::vec3(0.5, 0.8f, 0.2f), TTF::TEXT_POSITION::LEFT));
         scoreHeight -= heightIncrementOffset;
     }
 }
