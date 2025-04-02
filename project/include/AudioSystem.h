647d0d59d5857434ddcbfa4ffc2efa3a63fab7b5
Jon <jonathan.mulyk@ucalgary.ca>
2025-04-01 22:00:28 -0600

    decrease volume of explosion sound

--- project/include/AudioSystem.h
+++ project/include/AudioSystem.h
@@ -49,7 +49,7 @@ public:
 	float carVolume = -10.0f;
 	float aiVolume = -5.0f;
 	float musicVolume = -30.0f;
-	float explosionVolume = 0.0f;
+	float explosionVolume = -8.0f;
 
 	std::string menuMusic = "project/assets/audio/menuMusic.wav";
 	std::string battleMusic = "project/assets/audio/battleMusic.wav";
