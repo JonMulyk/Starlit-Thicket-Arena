d245c0779b1b25a8b0cc5c9574e669e4fd83e2e7
DuroMonstahCardo <efren.garcia6969@gmail.com>
2025-04-07 11:30:48 -0600

    background rendered class

--- project/include/MainMenu.h
+++ project/include/MainMenu.h
@@ -5,13 +5,14 @@
 #include <Button.h>
 #include "Controller.h" 
 #include <ControlMenu.h>
+//#include "BackgroundRenderer.h"
+#include <BackgroundRenderer.h>
+
 
 /*
 TODO: organize to seperate from cpp and .h file, essentially optimize and clean up after testing
 
 currently frontloaded in .h for functionality sakes (none of this code is really reusable)
-
-bug: unless we reinitialize the audio, when the game resets, the music does not play
 */
 class MainMenu {
 public:
@@ -22,13 +23,12 @@ public:
         exitButton(0, 0, 0, 0, glm::vec3(0, 0, 0))
     {
         compileShaders();
-        loadBackgroundTexture();
         initializeUIText();
         //initializeButtons(); 
     }
 
     ~MainMenu() {
-        glDeleteTextures(1, &backgroundTexture);
+        delete backgroundRenderer;
         uiText.clear();
         delete shader;
     }
@@ -42,6 +42,7 @@ public:
         }
         glfwSetInputMode(window.getGLFWwindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
         bool inMenu = true;
+        backgroundRenderer = new BackgroundRenderer("project/assets/background/backgrounMainMenu.jpg", shader);
 
         audio.startMenuMusic();  
         while (inMenu && !window.shouldClose()) {
@@ -79,6 +80,7 @@ private:
     Windowing& window;
     Shader* shader;
     unsigned int backgroundTexture;
+    BackgroundRenderer* backgroundRenderer;
     TTF& textRenderer;
     int windowWidth, windowHeight;
     Button startButton, controlsButton, exitButton;
@@ -92,7 +94,8 @@ private:
 
     void renderMenu() {
         glDisable(GL_DEPTH_TEST);
-        drawBackground();
+        //drawBackground();
+        backgroundRenderer->draw();
 
         glfwGetWindowSize(window.getGLFWwindow(), &windowWidth, &windowHeight);
 
@@ -158,66 +161,6 @@ private:
         shader = new Shader("MainMenuShader", "project/assets/shaders/mainMenuShader.vert", "project/assets/shaders/mainMenuShader.frag");
     }
 
-    void drawBackground() {
-        float vertices[] = {
-            // positions     // texture coords
-            -1.0f,  1.0f,    0.0f, 1.0f,  // top-left
-            -1.0f, -1.0f,    0.0f, 0.0f,  // bottom-left
-             1.0f, -1.0f,    1.0f, 0.0f,  // bottom-right
-            -1.0f,  1.0f,    0.0f, 1.0f,  // top-left
-             1.0f, -1.0f,    1.0f, 0.0f,  // bottom-right
-             1.0f,  1.0f,    1.0f, 1.0f   // top-right
-        };
-
-        unsigned int VBO, VAO;
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-        glEnableVertexAttribArray(1);
-
-        shader->use();
-        shader->setInt("useTexture", 1);
-        glBindTexture(GL_TEXTURE_2D, backgroundTexture);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glBindVertexArray(0);
-        glDeleteBuffers(1, &VBO);
-        glDeleteVertexArrays(1, &VAO);
-    }
-
-    void loadBackgroundTexture() {
-        glGenTextures(1, &backgroundTexture);
-        glBindTexture(GL_TEXTURE_2D, backgroundTexture);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        int width, height, nrChannels;
-        stbi_set_flip_vertically_on_load(true);
-        unsigned char* data = stbi_load("project/assets/background/backgrounMainMenu.jpg", &width, &height, &nrChannels, 0);
-        if (data) {
-            GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
-            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
-        }
-        else {
-            printf("Failed to load background texture\n");
-        }
-        stbi_image_free(data);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        stbi_set_flip_vertically_on_load(false);
-    }
 
     void handleKeyboardInput(bool& inMenu) {
         static bool keyUpPressed = false;
@@ -261,44 +204,45 @@ private:
     }
 
     void handleControllerInput(bool& inMenu) {
-        static bool dpadUpPressed = false; 
+        static bool dpadUpPressed = false;
         static bool dpadDownPressed = false;
         static bool aButtonPressed = false;
 
         if (!controller.isConnected()) return;
 
-
         if (!controller.isButtonPressed(XINPUT_GAMEPAD_DPAD_UP) && dpadUpPressed) {
-            currentSelection = (currentSelection == 0) ? 1 : 0;
-            dpadUpPressed = false;  
+            currentSelection = (currentSelection - 1 + 3) % 3; // 2 -> 1 -> 0
+            dpadUpPressed = false;
         }
         if (controller.isButtonPressed(XINPUT_GAMEPAD_DPAD_UP)) {
-            dpadUpPressed = true; 
+            dpadUpPressed = true;
         }
-
 
         if (!controller.isButtonPressed(XINPUT_GAMEPAD_DPAD_DOWN) && dpadDownPressed) {
-            currentSelection = (currentSelection == 1) ? 0 : 1;
-            dpadDownPressed = false;  
+            currentSelection = (currentSelection + 1) % 3; // 0 -> 1 -> 2
+            dpadDownPressed = false;
         }
         if (controller.isButtonPressed(XINPUT_GAMEPAD_DPAD_DOWN)) {
-            dpadDownPressed = true;  
+            dpadDownPressed = true;
         }
-
 
         if (!controller.isButtonPressed(XINPUT_GAMEPAD_A) && aButtonPressed) {
-            if (currentSelection == 0) {
-                inMenu = false; 
+            if (currentSelection == 0) { // Start
+                inMenu = false;
                 audio.stopMusic();
             }
-            if (currentSelection == 1) {
-                glfwSetWindowShouldClose(window.getGLFWwindow(), true);  
+            else if (currentSelection == 1) { // Controls
+                ControlsMenu controlsMenu(window, textRenderer, controller, shader);
+                controlsMenu.display();
+            }
+            else if (currentSelection == 2) { // Exit
+                glfwSetWindowShouldClose(window.getGLFWwindow(), true);
                 audio.stopMusic();
             }
-            aButtonPressed = false;  
+            aButtonPressed = false;
         }
         if (controller.isButtonPressed(XINPUT_GAMEPAD_A)) {
-            aButtonPressed = true; 
+            aButtonPressed = true;
         }
     }
 
